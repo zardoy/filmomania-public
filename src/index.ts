232c6d3e489f9537d3a6f3262693bcf4a9f84729
Vitaly Turovsky <vital2580@icloud.com>
2021-02-15 07:09:30 +0300

    standard boilerplate

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable
+//THIS IS ENTRY POINT ONLY FOR CRA (renderer side), NOT FOR ELECTRON!
+//TODO: change this behavior (I think it can be done with react-app-rewired)
+
+import "./react";
