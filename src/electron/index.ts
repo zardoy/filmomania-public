f1a519dea05b5c665e0fc1bbcce71f1b3104e43b
Vitaly Turovsky <vital2580@icloud.com>
2021-02-26 17:50:55 +0300

    add electron-debug

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -1,9 +1,13 @@
 import "./handleErrors";
 
+// todo-high electron-reloader
 import { app } from "electron";
+import electronDebug from "electron-debug";
 
 import { bindIPC } from "./ipc";
 import { createMainWindow } from "./mainWindow";
+
+electronDebug();
 
 export const debug = console.log;
 
