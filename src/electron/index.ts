781d432c88db872853724533580a4802ec628d4f
Vitaly Turovsky <vital2580@icloud.com>
2021-02-26 16:47:53 +0300

    remove support for ace connector

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -1,10 +1,7 @@
-import { AceConnector } from "ace-connector";
 import { app } from "electron";
 
 import { bindIPC } from "./ipc";
 import { createMainWindow } from "./mainWindow";
-
-export const aceConnector = process.platform === "win32" ? new AceConnector() : null;
 
 export const debug = console.log;
 
--- src/electron/ipc.ts
+++ src/electron/ipc.ts
@@ -5,40 +5,6 @@ import { requestTorrentsList } from "./requests/torrentsList";
 import { bindIPC as bindSettingsIPC, getAppSetting } from "./settings";
 import { bindIPCEvents, isSodaPlayerInstalled, playWithSodaPlayer } from "./sodaPlayer";
 
-const onFirstLaunch = async () => {
-    // let installedAceStreamVersion: string | null = null;
-    // if (aceConnector) {
-    //     try {
-    //         await aceConnector.connect();
-    //         // todo-low simplify it
-    //         if (aceConnector.engine.status === "connected") {
-    //             installedAceStreamVersion = aceConnector.engine.version;
-    //         }
-    //     } catch (err) {
-    //         if (
-    //             err instanceof ConnectionError &&
-    //             err.type === "ACE_ENGINE_NOT_INSTALLED"
-    //         ) {
-    //             installedAceStreamVersion = null;
-    //         } else {
-    //             throw err;
-    //         }
-    //     }
-    // }
-    // // players
-    // typedIpcMain.sendToWindow(mainWindow, "firstRunSpecs", {
-    //     installedAceStreamVersion,
-    //     installedPlayers: []
-    // });
-    // await new Promise<void>(resolve => {
-    //     typedIpcMain.addEventListener("setupFirstLaunch", (_, { defaultPlayerIndex }) => {
-    //         typedIpcMain.removeAllListeners("setupFirstLaunch");
-    //         resolve();
-    //         // electronSettings.setSync("defaultPlayer", )
-    //     });
-    // });
-};
-
 export const bindIPC = () => {
     typedIpcMain.handleAllRequests({
         appInit: async () => {
