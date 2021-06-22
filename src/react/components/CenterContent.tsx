637ad1be2c3f2a1d5ae770cbef0a1070aebed068
Vitaly Turovsky <vital2580@icloud.com>
2021-06-22 07:43:13 +0300

    add tailwind css

--- src/react/components/CenterContent.tsx
+++ src/react/components/CenterContent.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 
-import { Grid } from "@material-ui/core";
-
 interface ComponentProps {
-    GridProps?: React.ComponentProps<typeof Grid>;
 }
 
 // todo-moderate component
-let CenterContent: React.FC<ComponentProps> = ({ children, GridProps }) =>
-    <Grid container direction="column" justify="center" alignItems="center" style={{ height: "100vh" }} {...GridProps}>
-        {children}
-    </Grid>;
+let CenterContent: React.FC<ComponentProps> = ({ children }) =>
+    <div className="flex justify-center flex-col items-center h-screen">{children}</div>;
 
 export default CenterContent;
